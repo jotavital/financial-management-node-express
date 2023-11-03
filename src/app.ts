cb285648f3f7ce819268c2fbb13897c2682f1318
jotavital <joaopedrovitaldoctum@gmail.com>
2023-11-02 23:58:13 -0300

    feat: :sparkles: cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import usersRoutes from '~/routes/users';
+import cors from 'cors';
 
 const app = express();
 
+app.use(
+    cors({
+        origin: '*',
+    })
+);
 app.use(express.json());
 
 app.use('/api/users', usersRoutes);
