72636dc3eeca722840d9a1b036de5e02dfc19f2a
jotavital <joaopedrovitaldoctum@gmail.com>
2023-11-12 19:48:50 -0300

    feat: :sparkles: retornando token jwt para autenticacao

--- src/repositories/auth.ts
+++ src/repositories/auth.ts
@@ -1,8 +1,10 @@
+import 'dotenv/config';
+import jwt from 'jsonwebtoken';
 import CustomError from '~/errors/CustomError';
 import User from '~/models/User';
-import { SignInData } from '~/types/signIn';
+import { SignInData, SignInResponse } from '~/types/signIn';
 
-export const signIn = async (data: SignInData) => {
+export const signIn = async (data: SignInData): Promise<SignInResponse> => {
     const user = await User.findOne({ email: data.email }).select('+password');
 
     if (!user) {
@@ -15,7 +17,20 @@ export const signIn = async (data: SignInData) => {
         throw new CustomError({ message: 'Senha incorreta', code: 401 });
     }
 
+    const jwtPayload = {
+        userId: user.id,
+    };
+
+    const token = jwt.sign(jwtPayload, String(process.env.JWT_SECRET_KEY), {
+        // TODO: testar valor menor e redirecionar no front
+        expiresIn: '2 days',
+    });
+
+    if (!token) {
+        throw new CustomError({ message: 'Erro ao gerar o token de acesso' });
+    }
+
     user.password = '';
 
-    return user;
+    return { user, token };
 };
--- src/types/signIn.ts
+++ src/types/signIn.ts
@@ -1,4 +1,11 @@
+import { UserProps } from '~/types/user';
+
 export interface SignInData {
     email: string;
     password: string;
+}
+
+export interface SignInResponse {
+    user: UserProps;
+    token?: string;
 }
